0dbab903dedada43cf63940842f53a006ab830fe
vaibhav8845 <vaibhavsonawane2005@gmail.com>
2024-10-04 16:40:24 +0530

    changes of code app js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Farming from './Components/Pages/Farming/Farming';
 import Footer from './Components/Footer/Footer';
+import Internship from './Components/Pages/Internship/Internship';
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
@@ -35,6 +36,7 @@ const App = () => {
         <Route path="/contact" element={<Contact />} />
         <Route path="/services" element={<Services />} />
         <Route path="/farming" element={<Farming />} />
+        <Route path="/internship" element={<Internship />} />
 
         <Route path="/home" element={<Home/>}/>
 
