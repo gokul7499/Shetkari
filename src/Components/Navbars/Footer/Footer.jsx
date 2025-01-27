3706eb9986448e8b7cdb0d12c1a1a56a8f5a14c1
gudaghers <gudaghers@gmail.com>
2025-01-27 12:42:21 +0530

    gokl

--- src/Components/Navbars/Footer/Footer.jsx
+++ src/Components/Navbars/Footer/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
   // Function to get the current visitor count from the API
   const getVisitorCount = async () => {
     try {
-      const response = await axios.get('https://website-backend-royal.onrender.com/api/visitor');
+      const response = await axios.get('https://website-backend-royal.onrender.com/api/visitor/count');
       setVisitorCount(response.data.visitorCount); // Assuming the API response is in the form { visitorCount: number }
     } catch (error) {
       console.error('Error fetching visitor count:', error);
@@ -23,8 +23,8 @@ const Footer = () => {
   // Function to increment the visitor count via API
   const incrementVisitorCount = async () => {
     try {
-      const response = await axios.post('https://website-backend-royal.onrender.com/api/visitor/count');
-      setVisitorCount(response.data.visitorCount); // Update visitor count with the new value
+      await axios.get('https://website-backend-royal.onrender.com/api/visitor'); // This will trigger the visit save
+      getVisitorCount(); // After saving, get the updated visitor count
     } catch (error) {
       console.error('Error incrementing visitor count:', error);
     }
